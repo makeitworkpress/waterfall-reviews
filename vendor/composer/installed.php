f0d774e8c06e817e2093d343bf771cd1d3fb0730
leichim <michielsite@hotmail.com>
2023-03-23 17:35:39 +0100

    Added vendor updater script

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'makeitworkpress/waterfall-reviews',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0926efd8770c27ce098ab6c7c01f747b63630e03',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'makeitworkpress/waterfall-reviews' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0926efd8770c27ce098ab6c7c01f747b63630e03',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'makeitworkpress/wp-updater' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'eafa352d1e90ccd85035ab0476d2720354ca8a5c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../makeitworkpress/wp-updater',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
